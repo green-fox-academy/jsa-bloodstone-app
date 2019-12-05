914350b611fdc1a982da2e9b49d65dd95b0a6bb1
Kyya <favoursmoe@gmail.com>
2019-12-05 23:30:27 +0800

    JSAB2-51, add icon assets for troop

--- frontend/src/Troop/TroopInformation.jsx
+++ frontend/src/Troop/TroopInformation.jsx
@@ -4,15 +4,28 @@ import {
   StyleSheet,
 } from 'react-native';
 import { CardView } from '../components';
-import commonStyles from '../common/styles';
 import troopAvatar from '../../assets/troop/troop-avatar.jpg';
+import attackIcon from '../../assets/troop/attack.png';
+import defenceIcon from '../../assets/troop/defence.png';
+import cookieIcon from '../../assets/troop/cookie.png';
 
 const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 15,
+    lineHeight: 20,
+  },
   troopAvatar: {
     width: 80,
     height: 80,
     marginRight: 24,
     borderRadius: 10,
+  },
+  fieldStyle: {
+    flexDirection: 'row',
+  },
+  iconStyle: {
+    width: 16,
+    height: 16,
   },
 });
 
@@ -21,9 +34,18 @@ function TroopInformation() {
     <CardView style={{ flexDirection: 'row', alignItems: 'center' }}>
       <Image style={styles.troopAvatar} source={troopAvatar} />
       <View>
-        <Text style={commonStyles.textStyle}>Attack: 35</Text>
-        <Text style={commonStyles.textStyle}>Defence: 35</Text>
-        <Text style={commonStyles.textStyle}>Sustenance: 23</Text>
+        <View style={styles.fieldStyle}>
+          <Text style={styles.textStyle}>Attack: 35</Text>
+          <Image resizeMode="contain" source={attackIcon} style={styles.iconStyle} />
+        </View>
+        <View style={styles.fieldStyle}>
+          <Text style={styles.textStyle}>Defence: 35</Text>
+          <Image resizeMode="contain" source={defenceIcon} style={styles.iconStyle} />
+        </View>
+        <View style={styles.fieldStyle}>
+          <Text style={styles.textStyle}>Sustenance: 23</Text>
+          <Image resizeMode="contain" source={cookieIcon} style={styles.iconStyle} />
+        </View>
       </View>
     </CardView>
   );
