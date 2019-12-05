599cdc9f9ac14f01c3828d5e728337922fa492a9
Hanqing Zhao <sherry9277@gmail.com>
2019-12-05 17:17:32 +0800

    JSAB2-4, finish basic menu components

---
+++ frontend/src/components/TouchableImage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Text,
+  Image,
+  View,
+  TouchableHighlight,
+  Dimensions,
+} from 'react-native';
+
+export default function TouchableImage({
+  src, name, onPress, isPressed,
+}) {
+  const windowsWidth = Dimensions.get('window').width;
+  const LABEL_SIZE = Math.round(windowsWidth / 500) * 10;
+
+  return (
+    <TouchableHighlight underlayColor="transparent" onPress={() => onPress(name)} style={{ width: '25%', height: '100%' }}>
+      <View style={{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: isPressed === name ? 'rgba(148, 148, 148, .8)' : 'rgba(255, 255, 255, .8)',
+      }}
+      >
+        <Image source={src} style={{ width: '60%', height: '62%' }} />
+        <Text style={{ color: '#55565a', fontSize: LABEL_SIZE }}>{ name }</Text>
+      </View>
+    </TouchableHighlight>
+  );
+}
+
+TouchableImage.propTypes = {
+  src: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  onPress: PropTypes.func,
+  isPressed: PropTypes.string,
+};
+
+TouchableImage.defaultProps = {
+  onPress: null,
+  isPressed: '',
+};
--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -1,50 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Text,
-  Image,
   View,
-  TouchableHighlight,
+  Text,
   Dimensions,
 } from 'react-native';
+import TouchableImage from './TouchableImage';
 import buttomImage1 from '../../assets/menu/Buildings.png';
 import buttomImage2 from '../../assets/menu/Troops.png';
 import buttomImage3 from '../../assets/menu/Battle.png';
 import buttomImage4 from '../../assets/menu/Leaderboard.png';
 
 export default function Menu() {
-  function handlePress() {
-    // TODO
-  }
+  const [isPressed, setPressed] = useState('Buildings');
 
-  const renderButton = (src, names) => (
-    <TouchableHighlight underlayColor="transparent" onPress={handlePress} style={{ width: '25%', height: '100%' }}>
-      <View style={{
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-      >
-        <Image source={src} style={{ width: '60%', height: '62%' }} />
-        <Text style={{ color: '#55565a' }}>{ names }</Text>
-      </View>
-    </TouchableHighlight>
-  );
+  useEffect(() => { }, [isPressed]);
+
+  function handlePress(name) {
+    setPressed(name);
+    // switch (isPressed) {
+    //   case 'Buildings':
+    //     // TODO
+    //     break;
+    //   case 'Troops':
+    //     // TODO
+    //     break;
+    //   case 'Battle':
+    //     // TODO
+    //     break;
+    //   case 'Leaderboard':
+    //     // TODO
+    //     break;
+    //   default:
+    //     setPressed('Buildings');
+    //     // TODO
+    // }
+  }
 
   return (
     <View style={{
       flexDirection: 'row',
-      top: 0,
       width: Dimensions.get('window').width * 0.9,
       height: Dimensions.get('window').width * 0.22,
       backgroundColor: 'rgba(255, 255, 255, .8)',
       borderRadius: 3,
     }}
     >
-      {renderButton(buttomImage1, 'Buildings')}
-      {renderButton(buttomImage2, 'Troops')}
-      {renderButton(buttomImage3, 'Battle')}
-      {renderButton(buttomImage4, 'Leaderboard')}
+      <TouchableImage src={buttomImage1} name="Buildings" onPress={handlePress} isPressed={isPressed} />
+      <TouchableImage src={buttomImage2} name="Troops" onPress={handlePress} isPressed={isPressed} />
+      <TouchableImage src={buttomImage3} name="Battle" onPress={handlePress} isPressed={isPressed} />
+      <TouchableImage src={buttomImage4} name="Leaderboard" onPress={handlePress} isPressed={isPressed} />
+
+      <Text style={{ color: 'white' }}>{isPressed}</Text>
     </View>
   );
 }
