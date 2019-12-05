bd1b3fcc63b1cc82cd7f5a33bb35f2757b574280
Clay Tang <Clay_Tang@epam.com>
2019-12-05 12:49:57 +0800

    JSAB2-52, create troops backend

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -9,6 +9,32 @@ app.get('/', (req, res) => {
   res.status(200).send('Hellow world');
 });
 
+const myTroops = {
+  troops: [
+    {
+      id: 1,
+      level: 1,
+      hp: 1,
+      attack: 1,
+      defence: 1,
+      started_at: 12345789,
+      finished_at: 12399999,
+    }, {
+      id: 2,
+      level: 1,
+      hp: 1,
+      attack: 1,
+      defence: 1,
+      started_at: 12345789,
+      finished_at: 12399999,
+    },
+  ],
+};
+
+app.get('/kingdom/troops', (req, res) => {
+  res.status(200).send(myTroops);
+});
+
 app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
   next(err);
