1b51632ba08bd6cf457b88e4d3df048ca7092fa0
Hanqing Zhao <sherry9277@gmail.com>
2019-12-06 17:05:02 +0800

    JSAB2-4, fix small bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 import { registerRootComponent } from 'expo';
 import Troop from './Troop';
+import Menu from './Menu';
 import commonStyles from './common/styles';
 
 function App() {
   return (
     <View style={commonStyles.container}>
+      <Menu />
       <Troop />
     </View>
   );
