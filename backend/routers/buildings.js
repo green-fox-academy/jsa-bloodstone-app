c6276539cabbbe392f5b42b2fcc3e2734de1ee2f
Kyya <favoursmoe@gmail.com>
2019-12-10 15:35:01 +0800

    JSAB2-44, one building backend

--- backend/routers/buildings.js
+++ backend/routers/buildings.js
@@ -22,10 +22,28 @@ const myBuildings = {
   ],
 };
 
+const mockedOneBuilding = {
+  id: 2,
+  type: 'farm',
+  level: 1,
+  hp: 1,
+  started_at: 12345789,
+  finished_at: 12399999,
+};
+
 function getBuildings(req, res) {
   return res.status(200).send(myBuildings);
 }
 
+function getBuildingById(req, res) {
+  const buildingId = Number.parseInt(req.params.buildingId, 10);
+  if (!buildingId) {
+    return res.sendStatus(400);
+  }
+  return res.status(200).send(mockedOneBuilding);
+}
+
 router.get('/', getBuildings);
+router.get('/:buildingId?', getBuildingById);
 
 module.exports = router;
--- backend/tests/buildings.test.js
+++ backend/tests/buildings.test.js
@@ -10,4 +10,30 @@ describe('Buildings', () => {
         .expect(200, done);
     });
   });
+
+  describe('GET /kingdom/buildings/', () => {
+    it('should return 200 when getting all buildings (with slash)', (done) => {
+      request(app)
+        .get('/kingdom/buildings/')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+  });
+
+  describe('GET /kingdom/buildings/non-number', () => {
+    it('should return 400 when the building id is not a number', (done) => {
+      request(app)
+        .get('/kingdom/buildings/non-number')
+        .expect(400, done);
+    });
+  });
+
+  describe('GET /kingdom/buildings/2', () => {
+    it('should return 200 when getting a building', (done) => {
+      request(app)
+        .get('/kingdom/buildings/2')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+  });
 });
