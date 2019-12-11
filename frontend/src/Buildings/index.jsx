8de8d7750ce34c7560042cdc070e72bc6dc0d225
Clay Tang <Clay_Tang@epam.com>
2019-12-11 10:00:08 +0800

    JSAB2-74, fix bugs and add tabBarIcon

--- frontend/src/Buildings/index.jsx
+++ frontend/src/Buildings/index.jsx
@@ -3,11 +3,9 @@ import { View, Text } from 'react-native';
 
 function Buildings() {
   return (
-    <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Mocked Buildings</Text>
-      </View>
-    </>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Mocked Buildings</Text>
+    </View>
   );
 }
 
--- frontend/src/Navigation/index.jsx
+++ frontend/src/Navigation/index.jsx
@@ -1,21 +1,35 @@
+import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Settings from '../Settings';
 import Buildings from '../Buildings';
 import Colors from '../common/colors';
 
 const TabNavigator = createBottomTabNavigator({
-  'My Kingdom': Buildings,
-  Settings,
+  'My Kingdom': {
+    screen: Buildings,
+    navigationOptions: {
+      tabBarIcon: () => <Ionicons name="ios-home" size={25} style={{ color: 'white' }} />,
+    },
+  },
+
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      tabBarIcon: () => <Ionicons name="md-settings" size={25} style={{ color: 'white' }} />,
+    },
+  },
 }, {
   initialRouteName: 'My Kingdom',
   order: ['My Kingdom', 'Settings'],
   tabBarOptions: {
-    activeTintColor: '#ffd700',
-    inactiveTintColor: 'white',
+    activeBackgroundColor: '#00695c',
+    tabStyle: {
+      padding: 0,
+    },
     labelStyle: {
-      fontSize: 18,
       fontWeight: 'bold',
-      paddingBottom: 10,
+      color: 'white',
     },
     style: {
       backgroundColor: Colors.tealColor,
