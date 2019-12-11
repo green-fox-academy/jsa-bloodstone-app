fec0ef155717413dc88011db7a5c56764a094194
Jialu Gu <msljtacslw@gmail.com>
2019-12-11 09:32:57 +0800

    JSAB2-75, add scroll view

--- frontend/src/Leaderboard/index.jsx
+++ frontend/src/Leaderboard/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import RowTopTen from './RowTopTen';
 
 function Leaderboard() {
   return (
-    <>
+    <ScrollView>
       <RowTopTen rank={1} username="fdas" gold={20} crown={10} />
       <RowTopTen rank={2} username="fdas" gold={20} crown={10} />
       <RowTopTen rank={3} username="fdas" gold={20} crown={10} />
-    </>
+    </ScrollView>
   );
 }
 
