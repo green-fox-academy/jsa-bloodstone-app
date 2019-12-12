f1e011ebfb4565a8b18e13643e31149c2d6d9812
Jialu Gu <msljtacslw@gmail.com>
2019-12-12 20:44:33 +0800

    JSAB2-67, fix troop info not updating

--- frontend/src/Troop/index.jsx
+++ frontend/src/Troop/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import {
   View, ActivityIndicator,
   Text, StyleSheet,
@@ -18,7 +19,23 @@ const styles = StyleSheet.create({
   },
 });
 
-function TroopLevels() {
+function TroopLevels({ level1TroopNum, level2TroopNum, level3TroopNum }) {
+  return (
+    <View style={styles.levelList}>
+      {level1TroopNum !== 0 && <TroopLevel level={1} troops={level1TroopNum} />}
+      {level2TroopNum !== 0 && <TroopLevel level={2} troops={level2TroopNum} />}
+      {level3TroopNum !== 0 && <TroopLevel level={3} troops={level3TroopNum} />}
+    </View>
+  );
+}
+
+TroopLevels.propTypes = {
+  level1TroopNum: PropTypes.number.isRequired,
+  level2TroopNum: PropTypes.number.isRequired,
+  level3TroopNum: PropTypes.number.isRequired,
+};
+
+function Troop() {
   const troops = useSelector((state) => state.troop.troops);
   const isLoading = useSelector((state) => state.troop.isLoading);
   const fetchError = useSelector((state) => state.troop.fetchError);
@@ -43,28 +60,22 @@ function TroopLevels() {
   const level1TroopNum = troops.filter((troop) => troop.level === 1).length;
   const level2TroopNum = troops.filter((troop) => troop.level === 2).length;
   const level3TroopNum = troops.filter((troop) => troop.level === 3).length;
+  const attack = level1TroopNum + level2TroopNum * 2 + level3TroopNum * 3;
+  const defence = level1TroopNum + level2TroopNum * 2 + level3TroopNum * 3;
+  const substenance = level1TroopNum + level2TroopNum + level3TroopNum;
 
-  return (
-    <>
-      {isLoading
-        ? <ActivityIndicator size="large" color="#10978b" />
-        : (
-          <View style={styles.levelList}>
-            {level1TroopNum !== 0 && <TroopLevel level={1} troops={level1TroopNum} />}
-            {level2TroopNum !== 0 && <TroopLevel level={2} troops={level2TroopNum} />}
-            {level3TroopNum !== 0 && <TroopLevel level={3} troops={level3TroopNum} />}
-          </View>
-        )}
-    </>
-
-  );
-}
-
-function Troop() {
   return (
     <View style={{ flexDirection: 'column' }}>
-      <TroopInformation />
-      <TroopLevels />
+      <TroopInformation
+        attack={attack}
+        defence={defence}
+        substenance={substenance}
+      />
+      <TroopLevels
+        level1TroopNum={level1TroopNum}
+        level2TroopNum={level2TroopNum}
+        level3TroopNum={level3TroopNum}
+      />
     </View>
   );
 }
