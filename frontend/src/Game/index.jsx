718949f37922c63e3d1eeae016a20272f952bc33
Jialu Gu <msljtacslw@gmail.com>
2019-12-16 15:59:27 +0800

    JSAB2-29, add resource to game

--- frontend/src/Game/index.jsx
+++ frontend/src/Game/index.jsx
@@ -3,11 +3,13 @@ import { View } from 'react-native';
 import styles from '../common/styles';
 import Menu from '../Menu';
 import MainView from '../MainView';
+import Resource from '../Resource';
 
 function Game() {
   return (
     <View style={styles.screenStyle}>
       <Menu />
+      <Resource />
       <MainView />
     </View>
   );
