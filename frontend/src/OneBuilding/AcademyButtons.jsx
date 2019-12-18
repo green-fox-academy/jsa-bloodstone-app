d5e093a360906a9738ad447a5ae8e16f853a40a8
Clay Tang <Clay_Tang@epam.com>
2019-12-18 17:08:46 +0800

    JSAB2-43, modify AcademyButtons css

--- frontend/src/OneBuilding/AcademyButtons.jsx
+++ frontend/src/OneBuilding/AcademyButtons.jsx
@@ -9,30 +9,11 @@ import { SubCardView } from '../common/components';
 import goldIcon from '../../assets/gold.png';
 import troopIcon from '../../assets/troop/troop.png';
 
-import colors from '../common/colors'
+import commonStyles from './styles';
 
 const styles = StyleSheet.create({
   container: {
     paddingVertical: 12,
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  textStyle: {
-    fontSize: 14,
-    color: colors.whiteColor,
-  },
-  imageStyle: {
-    width: 32,
-    height: 32,
-  },
-  iconStyle: {
-    width: 16,
-    height: 16,
-  },
-  description: {
-    marginLeft: 12,
   },
 });
 
@@ -41,13 +22,13 @@ function AcademyButtons({ createTroops, upgrade }) {
     <View style={styles.container}>
       <TouchableHighlight onPress={createTroops} style={{ padding: 3 }}>
         <SubCardView>
-          <Image resizeMode="contain" source={troopIcon} style={styles.imageStyle} />
-          <View style={styles.description}>
-            <Text style={styles.textStyle}>Create troop level 1</Text>
-            <View style={styles.row}>
-              <Text style={styles.textStyle}>10 </Text>
-              <Image resizeMode="contain" source={goldIcon} style={styles.iconStyle} />
-              <Text style={styles.textStyle}> 1:00</Text>
+          <Image resizeMode="contain" source={troopIcon} style={commonStyles.imageStyle} />
+          <View style={commonStyles.description}>
+            <Text style={commonStyles.buttonTextStyle}>Create troop level 1</Text>
+            <View style={commonStyles.row}>
+              <Text style={commonStyles.buttonTextStyle}>10 </Text>
+              <Image resizeMode="contain" source={goldIcon} style={commonStyles.iconStyle} />
+              <Text style={commonStyles.buttonTextStyle}> 1:00</Text>
             </View>
           </View>
         </SubCardView>
@@ -57,12 +38,12 @@ function AcademyButtons({ createTroops, upgrade }) {
           <View>
             <Entypo name="arrow-with-circle-up" size={32} color="white" />
           </View>
-          <View style={styles.description}>
-            <Text style={styles.textStyle}>Upgrade to Level 2</Text>
-            <View style={styles.row}>
-              <Text style={styles.textStyle}>200 </Text>
-              <Image resizeMode="contain" source={goldIcon} style={styles.iconStyle} />
-              <Text style={styles.textStyle}> 2:30</Text>
+          <View style={commonStyles.description}>
+            <Text style={commonStyles.buttonTextStyle}>Upgrade to Level 2</Text>
+            <View style={commonStyles.row}>
+              <Text style={commonStyles.buttonTextStyle}>200 </Text>
+              <Image resizeMode="contain" source={goldIcon} style={commonStyles.iconStyle} />
+              <Text style={commonStyles.buttonTextStyle}> 2:30</Text>
             </View>
           </View>
         </SubCardView>
