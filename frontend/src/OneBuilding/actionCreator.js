5ecc4eef052c7f3169002122d4f61cd4894d5f2e
Clay Tang <Clay_Tang@epam.com>
2019-12-18 16:42:45 +0800

    JSAB2-43, put URL outside fetching function

--- frontend/src/OneBuilding/actionCreator.js
+++ frontend/src/OneBuilding/actionCreator.js
@@ -4,10 +4,12 @@ export const FETCH_ONE_BUILDING_REQUEST = 'fetchOneBuildingRequest';
 export const FETCH_ONE_BUILDING_SUCCESS = 'fetchOneBuildingSuccess';
 export const FETCH_ONE_BUILDING_FAILURE = 'fetchOneBuildingFailure';
 
+const URL = `http://${SERVER_URL}/kingdom/buildings/`;
+
 export function fetchOneBuilding(buildingId) {
   return (dispatch) => {
     dispatch({ type: FETCH_ONE_BUILDING_REQUEST });
-    fetch(`http://${SERVER_URL}/kingdom/buildings/${buildingId}`)
+    fetch(`${URL}${buildingId}`)
       .then((response) => {
         if (response.status === 200) {
           return response.json();
