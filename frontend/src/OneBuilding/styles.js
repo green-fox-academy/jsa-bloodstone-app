8e1743df029bc468a714f51709257ee92824fa78
Clay Tang <Clay_Tang@epam.com>
2019-12-18 17:08:11 +0800

    JSAB2-43, add styles.js for reusable css styles

---
+++ frontend/src/OneBuilding/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+import colors from '../common/colors';
+
+export default StyleSheet.create({
+  iconStyle: {
+    width: 16,
+    height: 16,
+  },
+  textStyle: {
+    lineHeight: 24,
+  },
+  imageStyle: {
+    width: 32,
+    height: 32,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  description: {
+    marginLeft: 12,
+  },
+  buttonTextStyle: {
+    color: colors.whiteColor,
+  },
+});
