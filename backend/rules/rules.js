7aea9d20ba325b39870b543c04d8f92cd1a000e8
Jialu Gu <msljtacslw@gmail.com>
2019-12-18 13:39:31 +0800

    JSAB2-93, change rules to be read from csv

--- backend/rules/rules.js
+++ backend/rules/rules.js
@@ -1,119 +1,25 @@
-const troopsRule = {
-  buildCost: 10,
-  buildSecond: 60,
-  level1: {
-    attack: 1,
-    defence: 1,
-    sustenance: 1,
-    upgradeCost: 20,
-    upgradeSecond: 120,
-  },
-  level2: {
-    attack: 2,
-    defence: 2,
-    sustenance: 1,
-    upgradeCost: 40,
-    upgradeSecond: 360,
-  },
-  level3: {
-    attack: 3,
-    defence: 3,
-    sustenance: 1,
-    upgradeCost: undefined,
-    upgradeSecond: undefined,
-  },
-};
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
 
-const buildingsRule = {
-  Townhall: {
-    buildCost: 100,
-    buildSecond: 60,
-    buildLimit: 1,
-    level1: {
-      upgradeCost: 250,
-      upgradeSecond: 1200,
-      maxBuildings: 4,
-    },
-    level2: {
-      upgradeCost: 400,
-      upgradeSecond: 3600,
-      maxBuildings: 8,
-    },
-    level3: {
-      upgradeCost: undefined,
-      upgradeSecond: undefined,
-      maxBuildings: 16,
-    },
-  },
+const buildingsRuleInput = fs.readFileSync(`${__dirname}/buildingsRules.csv`);
+const buildingsRule = parse(buildingsRuleInput, {
+  columns: true,
+});
+const townhallRule = buildingsRule.filter((building) => building.buildingName === 'townhall')[0];
+const farmRule = buildingsRule.filter((building) => building.buildingName === 'farm')[0];
+const mineRule = buildingsRule.filter((building) => building.buildingName === 'mine')[0];
+const academyRule = buildingsRule.filter((building) => building.buildingName === 'academy')[0];
 
-  Academy: {
-    buildCost: 100,
-    buildSecond: 60,
-    level1: {
-      upgradeCost: 250,
-      upgradeSecond: 1200,
-      troopsNumLimit: 10,
-      troopsLevelLimit: 1,
-    },
-    level2: {
-      upgradeCost: 400,
-      upgradeSecond: 3600,
-      troopsNumLimit: 20,
-      troopsLevelLimit: 2,
-    },
-    level3: {
-      upgradeCost: undefined,
-      upgradeSecond: undefined,
-      troopsNumLimit: 30,
-      troopsLevelLimit: 3,
-    },
-  },
-
-  Farm: {
-    buildCost: 100,
-    buildSecond: 60,
-    generationType: 'food',
-    level1: {
-      upgradeCost: 250,
-      upgradeSecond: 1200,
-      generationRate: 1,
-    },
-    level2: {
-      upgradeCost: 400,
-      upgradeSecond: 3600,
-      generationRate: 2,
-    },
-    level3: {
-      upgradeCost: undefined,
-      upgradeSecond: undefined,
-      generationRate: 5,
-    },
-
-  },
-
-  Mine: {
-    buildCost: 100,
-    buildSecond: 60,
-    generationType: 'gold',
-    level1: {
-      upgradeCost: 250,
-      upgradeSecond: 1200,
-      generationRate: 2,
-    },
-    level2: {
-      upgradeCost: 400,
-      upgradeSecond: 3600,
-      generationRate: 5,
-    },
-    level3: {
-      upgradeCost: undefined,
-      upgradeSecond: undefined,
-      generationRate: 10,
-    },
-  },
-};
+const troopsRuleInput = fs.readFileSync(`${__dirname}/troopsRules.csv`);
+const troopsRule = parse(troopsRuleInput, {
+  columns: true,
+});
+const foxRule = troopsRule.filter((troop) => troop.troopName === 'fox')[0];
 
 module.exports = {
-  troopsRule,
-  buildingsRule,
+  townhallRule,
+  farmRule,
+  mineRule,
+  academyRule,
+  foxRule,
 };
