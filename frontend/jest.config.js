f6fbf76066b33180157dbd48ff26cfd8cf809737
Kyya <favoursmoe@gmail.com>
2019-12-21 18:23:27 +0800

    JSAB2-119, add test coverage

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -7,4 +7,12 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.config.js',
+  ],
 };
