e4bc8ea86393a10ca2568509bff56b57f81c3783
Kyya <favoursmoe@gmail.com>
2019-12-30 22:58:27 +0800

    JSAB2, add onpress event to item view

--- frontend/src/Playground/ItemView.jsx
+++ frontend/src/Playground/ItemView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  View, Text, StyleSheet,
+  View, Text, StyleSheet, TouchableWithoutFeedback,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -8,33 +8,36 @@ const styles = StyleSheet.create({
   item: {
     padding: 12,
     alignItems: 'center',
-    // borderWidth: 1,
     flexBasis: 90,
   },
   textStyle: {
     marginTop: 6,
     fontSize: 12,
-    // fontWeight: 'bold',
+    fontFamily: 'Roboto',
   },
 });
 
-function ItemView({ title, icon }) {
+function ItemView({ title, icon, onPress }) {
   return (
-    <View style={styles.item}>
-      {icon}
-      <Text numberOfLines={1} style={styles.textStyle}>{title}</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.item}>
+        {icon}
+        <Text numberOfLines={1} style={styles.textStyle}>{title}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 ItemView.propTypes = {
   title: PropTypes.string,
   icon: PropTypes.node,
+  onPress: PropTypes.func,
 };
 
 ItemView.defaultProps = {
   title: 'Item',
   icon: null,
+  onPress: null,
 };
 
 export default ItemView;
--- frontend/src/Playground/index.jsx
+++ frontend/src/Playground/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-  View, SafeAreaView, StyleSheet,
+  SafeAreaView, StyleSheet,
 } from 'react-native';
 import { ScrollView } from 'react-navigation';
+import { useNavigation } from 'react-navigation-hooks';
 import {
-  Ionicons, AntDesign, Feather,
-  MaterialCommunityIcons,
+  Ionicons, AntDesign, Entypo, Foundation,
 } from '@expo/vector-icons';
 import commonStyles from '../common/styles';
 import PanelView from './PanelView';
@@ -17,32 +17,35 @@ const styles = StyleSheet.create({
     flex: 1,
     ...commonStyles.container,
     backgroundColor: 'rgba(0,0,0,0.05)',
-    // borderWidth: 1,
-  },
-  scrollViewStyle: {
-    // borderWidth: 1,
-    // borderColor: 'red',
   },
 });
 
 function Playground() {
+  const navigation = useNavigation();
+  function handleLogin() {
+    navigation.push('Login');
+  }
   return (
-    // <SafeAreaView style={{ flex: 1, borderWidth: 1 }}>
-    <View style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={styles.container}>
         <PanelView title="广场">
           <ItemView title="我的收藏" icon={<AntDesign size={28} name="star" color={Colors.orangeColor} />} />
           <ItemView title="我的收藏" icon={<Ionicons size={28} name="ios-heart" color={Colors.redColor} />} />
           <ItemView title="我的星球" icon={<Ionicons size={28} name="md-planet" color={Colors.blueColor} />} />
         </PanelView>
-        <PanelView title="三连哦亲">
-          <ItemView title="点赞" icon={<Ionicons size={28} name="md-thumbs-up" color={Colors.tealColor} />} />
-          <ItemView title="投币" icon={<MaterialCommunityIcons size={28} name="coin" />} />
-          <ItemView title="收藏" icon={<AntDesign size={28} name="star" color={Colors.orangeColor} />} />
+        <PanelView title="Test">
+          <ItemView
+            title="Map"
+            icon={<Foundation size={28} name="map" color={Colors.greenColor} />}
+          />
+          <ItemView
+            title="Logout"
+            icon={<Entypo size={28} name="log-out" color={Colors.blueColor} />}
+            onPress={handleLogin}
+          />
         </PanelView>
-        {/* <PanelView title="广场" /> */}
       </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 }
 
