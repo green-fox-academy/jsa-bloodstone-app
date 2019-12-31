cf835e630740148d534cd16a96595bac263ea96a
Kyya <favoursmoe@gmail.com>
2019-12-31 09:46:46 +0800

    JSAB2-89, fix verification function

--- frontend/src/Login/actionCreator.js
+++ frontend/src/Login/actionCreator.js
@@ -1,7 +1,5 @@
 export const LOGIN = 'login';
-export const FORGOT_PASSWORD_USERNAME = 'forgotPasswordUsername';
-export const FORGOT_PASSWORD_EMAIL = 'forgotPasswordEmail';
-export const FORGOT_PASSWORD_INVALID_ENTER = 'forgotPasswordInvalidEnter';
+export const FORGOT_PASSWORD = 'forgotPassword';
 
 export function login() {
   return {
