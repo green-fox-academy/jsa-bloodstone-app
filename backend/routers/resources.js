dcbd31398da07561e0f86c3482fb0fd0fc91e943
GuJialu <35847476+GuJialu@users.noreply.github.com>
2020-01-01 18:02:50 +0800

    JSAB2-106 Setup resource model (#52)
    * JSAB2-106, add resources model
    
    * JSAB2-106, update router
    
    * JSAB2-106, add destructing assign in map function
    
    * JSAB2-106, fix error

--- backend/routers/resources.js
+++ backend/routers/resources.js
@@ -1,39 +1,20 @@
 const { Router } = require('express');
+const { ResourceModel } = require('../models');
 
 const router = Router();
 
-const mockedResources = {
-  resources: [
-    {
-      type: 'food',
-      amount: 1,
-      generation: 60,
-      updatedAt: 1353647,
-    },
-    {
-      type: 'gold',
-      amount: 1,
-      generation: 60,
-      updatedAt: 1235346,
-    },
-  ],
-};
-
-function getResourceAmount(initialAmount, generation, updatedAt) {
-  const date = new Date();
-  const minutesPassed = (date.getTime() - updatedAt) / 60000;
-  return Math.round(initialAmount + generation * minutesPassed);
-}
-
-function getResources(req, res) {
-  const calculatedResources = {
-    resources: mockedResources.resources.map((resource) => ({
-      type: resource.type,
-      amount: getResourceAmount(resource.amount, resource.generation, resource.updatedAt),
-      generation: resource.generation,
-    })),
-  };
-  return res.status(200).send(calculatedResources);
+async function getResources(req, res, next) {
+  try {
+    const result = await ResourceModel.find({
+      owner: 1,
+    });
+    const resultFiltered = result.map(
+      ({ type, generation, amount }) => ({ type, generation, amount }),
+    );
+    res.status(200).send({ resources: resultFiltered });
+  } catch (error) {
+    next(error);
+  }
 }
 
 router.get('/', getResources);
