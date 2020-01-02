9373f8b50e2d66b065199b941e5e2af40dddff4b
Clay Tang <Clay_Tang@epam.com>
2020-01-02 11:58:45 +0800

    JSAB2-88, apply a loading screen

--- frontend/src/AuthLoadingScreen/index.jsx
+++ frontend/src/AuthLoadingScreen/index.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import {
-  ActivityIndicator,
-  View,
+  View, Image, StyleSheet,
 } from 'react-native';
 import { useNavigation } from 'react-navigation-hooks';
+import loadingIcon from '../../assets/loadingIcon.gif';
+import loadingKnight from '../../assets/loadingKnight.gif';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(65,65,65)',
+  },
+});
 
 function AuthLoadingScreen() {
   const navigation = useNavigation();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  if (isLoggedIn) {
-    return navigation.navigate('Home');
-  }
-  if (!isLoggedIn) {
+  setTimeout(() => {
+    if (isLoggedIn) {
+      return navigation.navigate('Home');
+    }
     return navigation.navigate('Auth');
-  }
+  }, 8000);
+
   return (
-    <View>
-      <ActivityIndicator />
+    <View style={styles.container}>
+      <Image style={{ height: 200, width: 300, marginBottom: 100 }} source={loadingKnight} />
+      <Image style={{ height: 50, width: 50 }} source={loadingIcon} />
     </View>
   );
 }
