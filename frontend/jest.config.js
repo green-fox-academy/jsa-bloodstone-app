6ab16ae000a797c1ddf72f2cd747688b8d9def4c
Kyya <favoursmoe@gmail.com>
2020-01-06 13:17:49 +0800

    JSAB2-119 Setup basic jest framework (#40)
    * JSAB2-119, setup basic jest framework
    
    * JSAB2-119, add test coverage

---
+++ frontend/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  verbose: true,
+  preset: 'jest-expo',
+  transform: {
+    '^.+\\.jsx$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.config.js',
+  ],
+};
