6d429d2edb69c6c265aa0ed94b135d40e6d479b0
Kyya <favoursmoe@gmail.com>
2020-01-06 09:30:06 +0800

    JSAB2-87, fix eslint error

--- frontend/src/Leaderboard/SearchBar.jsx
+++ frontend/src/Leaderboard/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {
   TextInput, StyleSheet,
 } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 import { CardView } from '../common/components';
 import colors from '../common/colors';
@@ -13,7 +13,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     borderRadius: 24,
-    // elevation: 0,
   },
   inputStyle: {
     flex: 1,
@@ -24,7 +23,6 @@ const styles = StyleSheet.create({
 
 function SearchBar({ onSubmit, placeholder }) {
   const [value, setValue] = useState('');
-
   return (
     <CardView style={styles.container}>
       <Ionicons name="md-search" size={20} color={colors.tealColor} />
