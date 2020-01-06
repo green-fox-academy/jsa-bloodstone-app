8a6310b651b3240f325b2e624a1b85b5efc97d25
hongtaoTang <hongtao.tang@my.jcu.edu.au>
2020-01-06 09:02:33 +0800

    JSAB2-115 Write test on menu (#65)
    * JSAB2-115, write test on actionCreator
    
    * JSAB2-115, write test on menuReducer, modify test on actionCreator
    
    * JSAB2-115, write test on index and MenuItem

---
+++ frontend/src/Menu/index.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Menu from '.';
+
+jest.mock('react-redux');
+jest.mock('./actionCreator');
+
+describe('<Menu />', () => {
+  it('Should render buildings and match the snapshot', () => {
+    const component = renderer.create(<Menu />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
