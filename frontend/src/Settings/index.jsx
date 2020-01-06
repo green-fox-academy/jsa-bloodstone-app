3a5f9b6e4264d703181fdb551c2b95b38e272997
Kyya <favoursmoe@gmail.com>
2020-01-06 23:25:41 +0800

    JSAB2-91, add a component to test notification

--- frontend/src/Settings/index.jsx
+++ frontend/src/Settings/index.jsx
@@ -1,39 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Notifications } from 'expo';
-import {
-  Text, Button, Alert, View,
-} from 'react-native';
-import { CardView } from '../common/components';
-import registerForPushNotificationsAsync from '../Notification/register';
+import React from 'react';
+import { View, Text } from 'react-native';
 import styles from '../common/styles';
-
-const textStyle = {
-  marginBottom: 12,
-  fontSize: 18,
-  fontWeight: 'bold',
-};
+import NotificationTestExample from '../Notification/NotificationTestExample';
 
 function Settings() {
-  const [token, setToken] = useState('');
-
-  useEffect(() => {
-    Notifications.getExpoPushTokenAsync()
-      .then((pushToken) => setToken(pushToken));
-  }, []);
-
-  function handlePress() {
-    registerForPushNotificationsAsync(
-      'Title', 'This is a test notification', {},
-    );
-    Alert.alert('INFO', 'sent');
-  }
   return (
-    <View style={styles.container}>
-      <CardView>
-        <Text style={textStyle}>Notification</Text>
-        <Button title="shot" onPress={handlePress} />
-        <Text>{token}</Text>
-      </CardView>
+    <View style={[styles.screenStyle,
+      { flex: 1, justifyContent: 'center', alignItems: 'center' }]}
+    >
+      <Text>Mocked Settings</Text>
+      <NotificationTestExample />
     </View>
   );
 }
