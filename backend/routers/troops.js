8df4e5d528b54304f55bf2e756301419f61ad0a8
Jialu Gu <msljtacslw@gmail.com>
2020-01-07 11:55:39 +0800

    JSAB2-126, add auth middleware in routers

--- backend/routers/troops.js
+++ backend/routers/troops.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const { TroopModel } = require('../models');
+const { auth } = require('../middlewares');
 
 const router = Router();
 
 async function getTroops(req, res, next) {
+  const { user } = req;
+  console.log(user);
   try {
-    const troops = await TroopModel.find({});
+    const troops = await TroopModel.find({ owner: user.id });
     res.send({ troops });
   } catch (error) {
     next(error);
@@ -13,6 +16,8 @@ async function getTroops(req, res, next) {
 }
 
 async function createTroop(req, res, next) {
+  const { user } = req;
+  console.log(user);
   const level = Number.parseInt(req.query.level, 10) || 1;
   const countByLevel = {
     level,
@@ -30,7 +35,7 @@ async function createTroop(req, res, next) {
   }
 }
 
-router.get('/', getTroops);
-router.post('/', createTroop);
+router.get('/', auth, getTroops);
+router.post('/', auth, createTroop);
 
 module.exports = router;
