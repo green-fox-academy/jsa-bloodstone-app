6d598ce1221595e1799af2e847a183f273d2a37f
Hanqing Zhao <sherry9277@gmail.com>
2020-01-07 22:13:25 +0800

    JSAB2-33, fix token decode error

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,7 @@ async function auth(req, res, next) {
   const bearerHeader = req.headers.authorization;
   try {
     const token = bearerHeader.split(' ')[1];
-    const { user } = jwt.verify(token, process.env.APP_SECRET);
+    const { user } = jwt.verify(token, process.env.APP_SECRET || 'testSecret');
     req.user = user;
     next();
   } catch (error) {
