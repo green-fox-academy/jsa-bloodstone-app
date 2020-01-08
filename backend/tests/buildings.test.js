4b0c6c77ae0fa4d227b9caa949ad64c81617042f
Jialu Gu <msljtacslw@gmail.com>
2020-01-08 19:40:18 +0800

    JSAB2-126, update test

--- backend/tests/buildings.test.js
+++ backend/tests/buildings.test.js
@@ -34,13 +34,4 @@ describe('Buildings', () => {
         .expect(400, done);
     });
   });
-
-  describe('GET /kingdom/buildings/2', () => {
-    it('should return 200 when getting a building', (done) => {
-      request(app)
-        .get('/kingdom/buildings/2')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
-  });
 });
--- backend/tests/troops.test.js
+++ backend/tests/troops.test.js
@@ -2,10 +2,23 @@ const request = require('supertest');
 const app = require('../App');
 
 describe('Troops', () => {
+  let token = null;
+
+  before((done) => {
+    request(app)
+      .post('/users/login')
+      .send({ username: 'testuser2', password: '12345678' })
+      .end((err, res) => {
+        token = res.body.token;
+        done();
+      });
+  });
+
   describe('GET /kingdom/troops', () => {
     it('should return 200 when getting all troops', (done) => {
       request(app)
         .get('/kingdom/troops')
+        .set('Authorization', `bearer ${token}`)
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
