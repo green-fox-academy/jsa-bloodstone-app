bf275cd91928609d8ddc3f390f7ea6df76101414
Kyya Wang <favoursmoe@gmail.com>
2020-01-09 11:29:58 +0800

    JSAB2-121 Refactor resource frontend (#54)
    * JSAB2-121, refactor resource frontend
    
    * JSAB2-121, fix some props error
    
    * JSAB2-121, put resource view to the top of menu
    
    * JSAB2-121, add menu title
    
    * JSAB2-121, fix bugs about gold too long(1000=1K)
    
    * JSAB2-121, add more situations about gold amount

--- frontend/src/Menu/index.jsx
+++ frontend/src/Menu/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { CardView } from '../common/components';
 import { changeDisplayedComponent } from './actionCreator';
+
 import MenuItem from './MenuItem';
+import Resources from '../Resources';
+
 import buildingMenuIcon from '../../assets/menu/Buildings.png';
 import troopsMenuIcon from '../../assets/menu/Troops.png';
 import battleMenuIcon from '../../assets/menu/Battle.png';
 import leaderBoardMenuIcon from '../../assets/menu/Leaderboard.png';
-import Colors from '../common/colors';
 
 const ICON_LIST = [
   { name: 'Buildings', url: buildingMenuIcon },
@@ -19,10 +21,24 @@ const ICON_LIST = [
 
 const styles = StyleSheet.create({
   container: {
+    padding: 0,
+    height: 150,
+  },
+  headerStyle: {
     flexDirection: 'row',
-    height: 120,
-    paddingVertical: 10,
-    backgroundColor: Colors.whiteColor,
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0,0,0,0.08)',
+    marginHorizontal: 10,
+  },
+  titleStyle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 10,
   },
 });
 
@@ -36,15 +52,23 @@ function Menu() {
 
   return (
     <CardView style={styles.container}>
-      {ICON_LIST.map((element) => (
-        <MenuItem
-          key={element.url}
-          src={element.url}
-          name={element.name}
-          onPress={handlePress}
-          activeComponent={activeComponent}
-        />
-      ))}
+      <View style={styles.headerStyle}>
+        <View style={{ paddingVertical: 7 }}>
+          <Text style={styles.titleStyle}>My Kingdom</Text>
+        </View>
+        <Resources />
+      </View>
+      <View style={styles.itemContainer}>
+        {ICON_LIST.map((element) => (
+          <MenuItem
+            key={element.url}
+            src={element.url}
+            name={element.name}
+            onPress={handlePress}
+            activeComponent={activeComponent}
+          />
+        ))}
+      </View>
     </CardView>
   );
 }
---
+++ frontend/src/Resources/ResourceItem.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  View, Image, StyleSheet, Text,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import Colors from '../common/colors';
+
+import cookieIcon from '../../assets/troop/cookie.png';
+import goldIcon from '../../assets/gold.png';
+
+const ICONS = {
+  gold: { type: 'gold', icon: goldIcon },
+  cookie: { type: 'cookie', icon: cookieIcon },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconStyle: {
+    width: 14,
+    height: 14,
+    marginHorizontal: 4,
+  },
+  text: {
+    fontSize: 14,
+    color: Colors.textColor,
+    fontWeight: 'bold',
+  },
+  subText: {
+    fontSize: 10,
+    color: Colors.greenColor,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
+const getRateText = (rate) => {
+  let rateStr;
+  let textColor;
+  if (rate < 0) {
+    rateStr = `${rate}/min`;
+    textColor = Colors.orangeColor;
+  } else {
+    rateStr = `+${rate}/min`;
+    textColor = Colors.greenColor;
+  }
+  return { rateStr, textColor };
+};
+
+function handleAmount(amount) {
+  if (amount >= 10000000) {
+    return `${Number.parseInt(amount / 1000000, 10)}M`;
+  }
+  if (amount >= 10000) {
+    return `${Number.parseInt(amount / 1000, 10)}K`;
+  }
+  return `${amount}`;
+}
+
+function ResourceItem({ type, amount, rate }) {
+  const { rateStr, textColor } = getRateText(rate);
+  const { icon } = ICONS[type];
+  return (
+    <View style={styles.container}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View style={styles.row}>
+          <Image style={styles.iconStyle} source={icon} />
+          <Text style={styles.text}>{handleAmount(amount)}</Text>
+        </View>
+        <View style={{ marginLeft: 4 }}>
+          <Text style={{ ...styles.subText, color: textColor }}>{rateStr}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+ResourceItem.propTypes = {
+  type: PropTypes.oneOf(Object.keys(ICONS)).isRequired,
+  amount: PropTypes.number.isRequired,
+  rate: PropTypes.number.isRequired,
+};
+
+export default ResourceItem;
