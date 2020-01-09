c9176ed417509b6664bb0a1adc04193a2d63ee2d
Kyya <favoursmoe@gmail.com>
2020-01-09 23:33:30 +0800

    JSAB2-canary, refactor add building view

--- frontend/src/Buildings/index.jsx
+++ frontend/src/Buildings/index.jsx
@@ -32,6 +32,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
+  addBuildingContainer: {
+    bottom: 0,
+    width: '100%',
+    paddingVertical: 10,
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.08)',
+  },
   textStyle: {
     fontSize: 15,
     lineHeight: 20,
@@ -89,17 +99,7 @@ function Buildings() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <CardView style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-around' }}>
-        {ADD_ICON_LIST.map((addBuildingIcon) => (
-          <AddBuildingItem
-            key={addBuildingIcon.type}
-            icon={addBuildingIcon.url}
-            type={addBuildingIcon.type}
-            onPress={() => addNewBuilding(addBuildingIcon.type)}
-          />
-        ))}
-      </CardView>
-      <CardView style={{ flex: 1, padding: 0, marginBottom: 0 }}>
+      <CardView style={{ flex: 1, padding: 0, marginBottom: 0, paddingBottom: 52 }}>
         <ScrollView
           bounces
           style={{ flex: 1 }}
@@ -114,18 +114,28 @@ function Buildings() {
             />
           ))}
         </ScrollView>
-        {
-          isModalVisible
-            ? (
-              <OneBuilding
-                targetBuildingId={activeId}
-                onClickClose={onCloseAddModal}
-                getIconImage={getIconImage}
-              />
-            )
-            : null
-        }
+        <View style={styles.addBuildingContainer}>
+          {ADD_ICON_LIST.map((addBuildingIcon) => (
+            <AddBuildingItem
+              key={addBuildingIcon.type}
+              icon={addBuildingIcon.url}
+              type={addBuildingIcon.type}
+              onPress={() => addNewBuilding(addBuildingIcon.type)}
+            />
+          ))}
+        </View>
       </CardView>
+      {
+        isModalVisible
+          ? (
+            <OneBuilding
+              targetBuildingId={activeId}
+              onClickClose={onCloseAddModal}
+              getIconImage={getIconImage}
+            />
+          )
+          : null
+      }
     </SafeAreaView>
   );
 }
