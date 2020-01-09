419af0794fed6c6c2845852afe98ba19cf8765de
Hanqing Zhao <sherry9277@gmail.com>
2020-01-09 14:43:48 +0800

    JSAB2-42, delete useless part

--- backend/routers/setup.js
+++ backend/routers/setup.js
@@ -8,15 +8,12 @@ const router = Router();
 async function chooseKingdom(req, res, next) {
   const { _id: owner } = req.user;
   const { kingdomList } = req.body;
-  console.log(kingdomList);
   try {
     if (!kingdomList) {
       throw createError(400, 'Please choose a kingdom to start your trip.');
     }
     const userInfo = await UserModel.findById(owner);
-    console.log(userInfo);
     userInfo.kingdomList.push(kingdomList);
-    console.log(userInfo);
     userInfo.save();
     req.owner = owner;
     res.sendStatus(200);
