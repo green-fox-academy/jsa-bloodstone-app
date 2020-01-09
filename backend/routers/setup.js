de3310465fb79b52cf3516aa49eaf15bcd355714
Hanqing Zhao <sherry9277@gmail.com>
2020-01-09 15:03:10 +0800

    JSAB2-42, add a new error checking

--- backend/routers/setup.js
+++ backend/routers/setup.js
@@ -13,6 +13,9 @@ async function chooseKingdom(req, res, next) {
       throw createError(400, 'Please choose a kingdom to start your trip.');
     }
     const userInfo = await UserModel.findById(owner);
+    if (userInfo.kingdomList.length === 0) {
+      throw createError(400, 'This kingdom list is not empty.');
+    }
     userInfo.kingdomList.push(kingdomList);
     userInfo.save();
     req.owner = owner;
