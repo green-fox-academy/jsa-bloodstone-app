910957686e6930015033afe030fc622e6ca5bcde
Clay Tang <Clay_Tang@epam.com>
2020-01-15 17:58:23 +0800

    JSAB2-15, set up createtroop action

--- frontend/src/Troops/actionCreator.js
+++ frontend/src/Troops/actionCreator.js
@@ -3,6 +3,9 @@ import { SERVER_URL } from 'react-native-dotenv';
 export const FETCH_TROOPS_SUCCESS = 'fetchTroopsSuccess';
 export const FETCH_TROOPS_REQUEST = 'fetchTroopsRequest';
 export const FETCH_TROOPS_FAILURE = 'fetchTroopsFailure';
+export const CREATE_TROOP_SUCCESS = 'createTroopSuccess';
+export const CREATE_TROOP_REQUEST = 'createTroopRequest';
+export const CREATE_TROOP_FAILURE = 'createTroopFailure';
 
 const URL = `http://${SERVER_URL}/kingdom/troops`;
 
@@ -23,5 +26,28 @@ export function fetchTroops(token) {
       })
       .then((response) => dispatch({ type: FETCH_TROOPS_SUCCESS, payload: response.troops }))
       .catch((error) => dispatch({ type: FETCH_TROOPS_FAILURE, payload: error }));
+  };
+}
+
+export function createTroop(level, token) {
+  return (dispatch) => {
+    dispatch({ type: CREATE_TROOP_REQUEST });
+    return fetch(`${URL}?level=${level}`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 400) {
+          return dispatch({ type: CREATE_TROOP_FAILURE, payload: response });
+        }
+        if (response.status === 201) {
+          return dispatch({ type: CREATE_TROOP_SUCCESS, payload: response.troops });
+        }
+        return null;
+      })
+      .catch((error) => dispatch({ type: CREATE_TROOP_FAILURE, payload: error }));
   };
 }
--- frontend/src/Troops/troopReducer.js
+++ frontend/src/Troops/troopReducer.js
@@ -1,4 +1,7 @@
-import { FETCH_TROOPS_SUCCESS, FETCH_TROOPS_REQUEST, FETCH_TROOPS_FAILURE } from './actionCreator';
+import {
+  FETCH_TROOPS_SUCCESS, FETCH_TROOPS_REQUEST, FETCH_TROOPS_FAILURE,
+  CREATE_TROOP_SUCCESS, CREATE_TROOP_REQUEST, CREATE_TROOP_FAILURE,
+} from './actionCreator';
 
 const initialState = {
   listOfTroops: [],
@@ -9,12 +12,15 @@ const initialState = {
 export default function troops(state = initialState, action) {
   switch (action.type) {
     case FETCH_TROOPS_REQUEST:
+    case CREATE_TROOP_REQUEST:
       return { ...state, isLoading: true };
     case FETCH_TROOPS_SUCCESS:
+    case CREATE_TROOP_SUCCESS:
       return {
         ...state, listOfTroops: action.payload, isLoading: false, error: undefined,
       };
     case FETCH_TROOPS_FAILURE:
+    case CREATE_TROOP_FAILURE:
       return { ...state, isLoading: false, error: action.payload };
     default:
       return state;
